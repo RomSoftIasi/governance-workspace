da995d844e62319f20a489059af9c58a27d6121d
bmastahac <bogdan.mastahac@gmail.com>
2021-02-12 16:14:11 +0200

    Integrating deploy cluster action with cluster template

--- clustersController/strategies/ControlContainer.js
+++ clustersController/strategies/ControlContainer.js
@@ -68,10 +68,48 @@ $$.flow.describe('ControlContainer', {
     },
     deployCluster: function (jsonData, callback) {
         console.log('deployCluster', jsonData);
-        return callback(undefined, {
+        this._executeDeployment({
+            "clusterName":"pl-cluster1",
+            "urlConfigRepo":"https://github.com/PharmaLedger-IMI/opendsu-cluster-template.git"
+        }, (err, result) => {
+            if (err)
+            {
+                return callback(err);
+            }
+            return callback(undefined, result);
+        })
+        /*return callback(undefined, {
             clusterName: jsonData.clusterName,
             urlConfigRepo: jsonData.urlConfigRepo,
             configMap: jsonData.configMap,
         });
+         */
     },
+    _executeDeployment : function (jsonData, callback)
+    {
+        const clusterTemplateRootFolder = require('../clusters.json').clusterTemplate.location;
+        const path = require('path');
+        const clusterMarker = "-".concat(jsonData.clusterName);
+        const repolink = jsonData.urlConfigRepo;
+        const parts = repolink.split('/');
+
+        const repoDir = parts[parts.length-1].replace('.git','');
+        console.log(repoDir);
+        const shellcmd = path.resolve(path.join(process.env.PSK_ROOT_INSTALATION_FOLDER, require('../clusters.json').clusterTemplate.shell));
+        //const shellcmd = path.join(path.resolve(),require('../clusters.json').clusterTemplate.shell) ;
+        const exec = require('child_process').exec;
+        const cmd = shellcmd.concat(" ",clusterMarker," ",jsonData.clusterName," ",clusterTemplateRootFolder," ",repolink," ",repoDir);
+        //shellcmd+' -pl-cluster1 pl-cluster1 '+clusterTemplateRootFolder,
+        exec(cmd, (err, stdout, stderr) => {
+            console.log('shell script finished');
+            console.log(stdout);
+            if (err)
+            {
+                console.log('shell execution failed.', err, stderr);
+                return callback(err);
+            }
+            console.log('Cluster deployed')
+            return callback(undefined, "Cluster deployed");
+        })
+    }
 });

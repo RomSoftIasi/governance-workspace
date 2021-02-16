c6cecfc9351150ff2d9172808526b8d7eaec6411
bmastahac <bogdan.mastahac@gmail.com>
2021-02-16 13:56:48 +0200

    Integrate ui actions with cluster deployment

--- clustersController/scripts/deploy-cluster.sh
+++ clustersController/scripts/deploy-cluster.sh
@@ -7,7 +7,13 @@ ROOT_PATH="${3}"
 COLLECTION_TEMPLATE_FOLDER="cluster-template-collection"
 REPO_LINK="${4}"
 REPO_NAME="${5}"
+LOGOUTPUT_DIR="${6}"
+LOG_CLUSTER_DIR="${REPO_NAME}${1}"
+#create directory for logs to be copied when the installation finishes
+cd ${LOGOUTPUT_DIR}
+if [ ! -d "${LOG_CLUSTER_DIR}" ]; then mkdir ${LOG_CLUSTER_DIR}; fi
 
+#prepare the environment for the configuration repo to be cloned and executed on required context
 cd ${ROOT_PATH}
 
 if [ ! -d "${COLLECTION_TEMPLATE_FOLDER}" ]; then mkdir ${COLLECTION_TEMPLATE_FOLDER}; fi
@@ -22,3 +28,5 @@ if [ ! -d "${REPO_NAME}" ]; then git clone ${REPO_LINK}; fi
 cd ${REPO_NAME}
 npm install
 npm run deploy-cluster -- ${CLUSTER_NAME}
+
+cp ./logs.txt "${LOGOUTPUT_DIR}/${LOG_CLUSTER_DIR}"

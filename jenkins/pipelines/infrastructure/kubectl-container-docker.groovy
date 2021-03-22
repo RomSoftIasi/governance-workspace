6769bb50ca62e900cdfc39d9fcca15de1d8fc8f4
bmastahac <bogdan.mastahac@gmail.com>
2021-03-22 13:29:10 +0200

    Jenkins kubectl container

--- jenkins/pipelines/infrastructure/kubectl-container-docker.groovy
+++ jenkins/pipelines/infrastructure/kubectl-container-docker.groovy
@@ -17,9 +17,6 @@ podTemplate(
                 stage ('Build'){
                     container('node'){
                         sh 'git clone http://github.com/PharmaLedger-IMI/governance-workspace.git'
-                        stage ('Build'){
-                            sh 'cd governance-workspace && npm run dev-install'
-                        }
                     }
                 }
 
@@ -29,7 +26,7 @@ podTemplate(
                         withCredentials([string(credentialsId: 'DOCKER_PASSWORD', variable: 'DOCKER_PASSWORD'), string(credentialsId: 'DOCKER_USERNAME', variable: 'DOCKER_USERNAME'), string(credentialsId: 'DOCKER_REGISTRY', variable: 'DOCKER_REGISTRY'), string(credentialsId: 'DOCKER_REPO', variable: 'DOCKER_REPO')]) {
                             sh 'docker login -p $DOCKER_PASSWORD -u $DOCKER_USERNAME $DOCKER_REGISTRY'
                             stage ('Build and push docker image'){
-                                  sh 'cd governance-workspace/jenkins/docker.kubectl && docker build --network host -t $DOCKER_REPO/pharma-kubectl .'
+                                  sh 'cd governance-workspace/jenkins/docker/kubectl && docker build --network host -t $DOCKER_REPO/pharma-kubectl .'
                                   sh 'docker push $DOCKER_REPO/pharma-kubectl'
                             }
 

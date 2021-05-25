d6d3f248dd61a9e7555240469a2280d9b2ec7bce
bmastahac <bogdan.mastahac@gmail.com>
2021-05-25 14:08:46 +0300

    updated eth adapter stage

--- jenkins/quorum-fresh-mode/stages/deployEthAdapter/deployEthAdapter.js
+++ jenkins/quorum-fresh-mode/stages/deployEthAdapter/deployEthAdapter.js
@@ -1,11 +1,15 @@
 function updateEthAdapterConfigMap(){
     const fs = require('fs');
     let ethAdapterYaml = fs.readFileSync('./k8s/ethAdapter/templates/ethadapter-configmap.yaml.template').toString();
-    const scData = JSON.parse(fs.readFileSync('./k8s/ethAdapter/templates/ethAdapterJoiningJSON.json').toString());
-    console.log(ethAdapterYaml);
+    const jsonBase64 = fs.readFileSync('./k8s/ethAdapter/templates/ethAdapterJoiningJSON.json',).toString();
+    const ethAdapterJson = Buffer.from(jsonBase64,'base64').toString('utf8');
+    console.log(ethAdapterJson);
+    const scData = JSON.parse(ethAdapterJson);
+
     ethAdapterYaml = ethAdapterYaml.replace('%SMARTCONTRACTADDRESS%',scData.contractAddress);
     ethAdapterYaml = ethAdapterYaml.replace('%SMARTCONTRACTABI%',JSON.stringify(scData.abi));
 
+    console.log(ethAdapterYaml);
     fs.writeFileSync('./k8s/ethAdapter/ethadapter-configmap.yaml',ethAdapterYaml);
     console.log('Eth Adapter config map file is updated.')
 }

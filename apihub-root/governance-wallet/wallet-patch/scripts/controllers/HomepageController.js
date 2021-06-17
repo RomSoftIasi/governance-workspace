1985edda7ec0e70c7c022c12a6321c51dd9b9ccb
paiusCatalin <p.cata95@gmail.com>
2021-06-17 16:23:50 +0300

    Created webcardinal wallet and eGovernance home page + design according to specifications

---
+++ apihub-root/governance-wallet/wallet-patch/scripts/controllers/HomepageController.js
@@ -0,0 +1,50 @@
+const {WebcController} = WebCardinal.controllers;
+
+export default class HomepageController extends WebcController {
+    constructor(...props) {
+        super(...props);
+
+        this.updateProfile();
+        this.initNavigationListeners();
+    }
+
+    initNavigationListeners() {
+        this.onTagClick('governance-dashboard', (model, target, event) => {
+            event.preventDefault();
+            event.stopImmediatePropagation();
+
+            this.navigateToPageTag("governance-dashboard");
+        });
+
+        this.onTagClick('organizations', (model, target, event) => {
+            event.preventDefault();
+            event.stopImmediatePropagation();
+
+            this.navigateToPageTag("organizations");
+        });
+
+        this.onTagClick('tools', (model, target, event) => {
+            event.preventDefault();
+            event.stopImmediatePropagation();
+
+            this.navigateToPageTag("tools");
+        });
+    }
+
+    updateProfile() {
+        const webcAppIdentityElement = document.querySelector("webc-app-identity");
+        if (!webcAppIdentityElement) {
+            return;
+        }
+
+        const logoElement = document.createElement("img");
+        logoElement.src = "/assets/images/pharma_logo.png";
+        logoElement.width = 100;
+
+        const linkElement = document.createElement("a");
+        linkElement.href = "/";
+        linkElement.append(logoElement);
+
+        webcAppIdentityElement.shadowRoot.innerHTML = linkElement.outerHTML;
+    }
+}

f6b27b9965da75fdad9fc88d6a64fa0657106b5d
bmastahac <bogdan.mastahac@gmail.com>
2021-07-08 12:11:03 +0300

    Updated clean pipeline to not fail when partial resources are removed

--- jenkins/pipelines/clean_usecase_installation.groovy
+++ jenkins/pipelines/clean_usecase_installation.groovy
@@ -1,4 +1,4 @@
-podTemplate(serviceAccount: 'jgovmns',namespace: 'jenkins',containers: [
+podTemplate(serviceAccount: 'jenkins',namespace: 'jenkins',containers: [
       containerTemplate(name: 'kubectl', image: 'public.ecr.aws/n4q1q0z2/pharmaledger-kubectl-jenkins-agent:1.0', command: 'cat', ttyEnabled: true)
     ],
     volumes: [
@@ -8,14 +8,56 @@ podTemplate(serviceAccount: 'jgovmns',namespace: 'jenkins',containers: [
 
             stage ('Cleanup usecase installation'){
                 container('kubectl'){
-                    sh 'kubectl delete deployments $subdomain -n default'
-                    sh 'kubectl delete services $domain -n default'
-                    sh 'kubectl delete configmaps $subdomain -n default'
-                    sh 'kubectl delete configmaps domains-$subdomain -n default'
-                    sh 'kubectl get deployments -n default'
-                    sh 'kubectl get pods  -n default'
-                    sh 'kubectl get services -n default'
-                    sh 'kubectl get configmaps -n default'
+                    try{
+                        stage('Remove deployment'){
+                            sh 'kubectl delete deployments $subdomain -n default'
+                        }
+                    }
+                    catch (err) {
+                        unstable (message: "${STAGE_NAME} is unstable.")
+                    }
+
+                    try{
+                        stage('Remove services'){
+                            sh 'kubectl delete services $domain -n default'
+                        }
+                    }
+                    catch (err) {
+                        unstable (message: "${STAGE_NAME} is unstable.")
+                    }
+
+                    try{
+                        stage('Remove config map'){
+                            sh 'kubectl delete configmaps $subdomain -n default'
+                        }
+                    }
+                    catch (err) {
+                        unstable (message: "${STAGE_NAME} is unstable.")
+                    }
+
+                    try{
+                        stage('Remove config map'){
+                             sh 'kubectl delete configmaps domains-$subdomain -n default'
+                        }
+                    }
+                    catch (err) {
+                        unstable (message: "${STAGE_NAME} is unstable.")
+                    }
+
+                    try{
+                        stage('Remove config map'){
+                            sh 'kubectl get deployments -n default'
+                            sh 'kubectl get pods  -n default'
+                            sh 'kubectl get services -n default'
+                            sh 'kubectl get configmaps -n default'
+                        }
+                    }
+                    catch (err) {
+                        unstable (message: "${STAGE_NAME} is unstable.")
+                    }
+
+
+
                 }
             }
 

f2865e54691f4de0d5fdd6bf716247007bee3205
bmastahac <bogdan.mastahac@gmail.com>
2021-09-20 16:38:47 +0300

    updated to freeze version

--- scripts/gov/build-gov.sh
+++ scripts/gov/build-gov.sh
@@ -2,6 +2,6 @@
 
 
 echo "Build and push Governance docker image"
-docker build --no-cache -t "$GOV_DOCKER_IMAGE":"$GOV_DOCKER_IMAGE_VERSION" -f ./docker/dev/Dockerfile .
+docker build --no-cache -t "$GOV_DOCKER_IMAGE":"$GOV_DOCKER_IMAGE_VERSION" -f ./docker/Dockerfile .
 docker tag "$GOV_DOCKER_IMAGE":"$GOV_DOCKER_IMAGE_VERSION" "$POD_DOCKER_REPOSITORY":"$GOV_DOCKER_IMAGE"_"$GOV_DOCKER_IMAGE_VERSION"
 docker push "$POD_DOCKER_REPOSITORY":"$GOV_DOCKER_IMAGE"_"$GOV_DOCKER_IMAGE_VERSION"
--- scripts/gov/clean-gov.sh
+++ scripts/gov/clean-gov.sh
@@ -4,20 +4,20 @@
 echo "Generate deployment files"
 
 
-sed "s/%gov-metadata-name%/$GOVCONTAINERNAME/g" docker/dev/k8s/templates/gov-deployment.yaml.template \
+sed "s/%gov-metadata-name%/$GOVCONTAINERNAME/g" docker/k8s/templates/gov-deployment.yaml.template \
 | sed "s/%gov-app-label%/$GOVAPPNAME/g" \
 | sed "s|%POD_DOCKER_REPOSITORY%|$POD_DOCKER_REPOSITORY|g" \
 | sed "s/%GOV_DOCKER_IMAGE%/$GOV_DOCKER_IMAGE/g" \
-| sed "s/%GOV_DOCKER_IMAGE_VERSION%/$GOV_DOCKER_IMAGE_VERSION/g"  > docker/dev/k8s/gov-deployment.yaml
-sed "s/%gov-metadata-name%/$GOVCONTAINERNAME/g" docker/dev/k8s/templates/gov-service.yaml.template | sed "s/%gov-app-label%/$GOVAPPNAME/g" > docker/dev/k8s/gov-service.yaml
+| sed "s/%GOV_DOCKER_IMAGE_VERSION%/$GOV_DOCKER_IMAGE_VERSION/g"  > docker/k8s/gov-deployment.yaml
+sed "s/%gov-metadata-name%/$GOVCONTAINERNAME/g" docker/k8s/templates/gov-service.yaml.template | sed "s/%gov-app-label%/$GOVAPPNAME/g" > docker/k8s/gov-service.yaml
 
 
 echo "Remove Governance from kubernetes cluster"
-kubectl delete -f ./docker/dev/k8s
+kubectl delete -f ./docker/k8s
 kubectl get pods
 
 echo "Clean up deployment files"
-rm docker/dev/k8s/gov-deployment.yaml
-rm docker/dev/k8s/gov-service.yaml
+rm docker/k8s/gov-deployment.yaml
+rm docker/k8s/gov-service.yaml
 
 

d7d3005909fc59b4c85bba7c544de67b24a9cfd4
bmastahac <bogdan.mastahac@gmail.com>
2021-11-05 12:27:52 +0200

    integration of shared quorum

--- jenkins/shared-quorum/start/generate-network.sh
+++ jenkins/shared-quorum/start/generate-network.sh
@@ -3,8 +3,8 @@ node ./stages/AdmAcc/stage.js
 node ./stages/Validators/stage.js --noOfNodes="$1" --joinMode=0
 
 cp ./k8s/templates/00-00-00-quorum-persistent-volumes.yaml.template ./k8s/00-00-00-quorum-persistent-volumes.yaml
-cp ./k8s/deployments/ ../start/k8s/deployments/
-cp ./k8s/ ../start/k8s/
+cp -f ./k8s/deployments/*.* ../start/k8s/deployments/
+cp -f ./k8s/*.* ../start/k8s/
 cp ./stages/Validators/validators.txt ../start/my-validators.txt
 cp ./stages/Validators/enodes.txt ../start/my-enodes.txt
 cp ./stages/AdmAcc/admAcc.json ../start/admAcc.json

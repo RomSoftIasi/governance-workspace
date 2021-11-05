d174dcc272691a8362ef5166ccb9afceb284ec3a
bmastahac <bogdan.mastahac@gmail.com>
2021-11-05 11:49:23 +0200

    integration of shared quorum

--- jenkins/shared-quorum/start/deploy-network.sh
+++ jenkins/shared-quorum/start/deploy-network.sh
@@ -1,13 +1,13 @@
 echo ''
 echo 'Blockchain network deployment'
 echo ''
-kubectl apply -f ./start/k8s
-kubectl apply -f ./start/k8s/deployments
+kubectl apply -f ./start/k8s -n "${0}"
+kubectl apply -f ./start/k8s/deployments -n "${0}"
 
 echo ''
 echo 'Blockchain explorer deployment'
 echo ''
-kubectl apply -f ./blockchain-explorer
+kubectl apply -f ./blockchain-explorer -n "${0}"
 
 sleep 30s
-kubectl get svc | awk '/quorum-node-/{print $1 " - " $2 " - " $3 " - " $4 " - " $5}' > ./start/my-node-service.txt
+kubectl get svc -n "${0}" | awk '/quorum-node-/{print $1 " - " $2 " - " $3 " - " $4 " - " $5}' > ./start/my-node-service.txt

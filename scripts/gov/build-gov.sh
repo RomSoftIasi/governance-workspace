6277a322e1b5707fb28a7ba555910e9fa6fb8fc0
bmastahac <bogdan.mastahac@gmail.com>
2022-02-09 15:19:40 +0200

    update dockerfile

--- scripts/gov/build-gov.sh
+++ scripts/gov/build-gov.sh
@@ -2,6 +2,6 @@
 
 
 echo "Build and push Governance docker image"
-docker build --no-cache -t "$GOV_DOCKER_IMAGE":"$GOV_DOCKER_IMAGE_VERSION" -f ./docker/Dockerfile .
+docker build --no-cache --build-arg GHCODE="$GITHUB_REPO_TOKEN" -t "$GOV_DOCKER_IMAGE":"$GOV_DOCKER_IMAGE_VERSION" -f ./docker/Dockerfile .
 docker tag "$GOV_DOCKER_IMAGE":"$GOV_DOCKER_IMAGE_VERSION" "$POD_DOCKER_REPOSITORY"/"$GOV_DOCKER_IMAGE":"$GOV_DOCKER_IMAGE_VERSION"
 docker push "$POD_DOCKER_REPOSITORY"/"$GOV_DOCKER_IMAGE":"$GOV_DOCKER_IMAGE_VERSION"
